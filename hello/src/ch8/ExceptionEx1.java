0ee95a4d5ff00e36e61998d2f579f2449f9d4681
ga29_ <ga29_@ga29>
2024-05-30 19:36:49 +0900

    2024.05.30 수업

--- hello/src/ch8/ExceptionEx1.java
+++ hello/src/ch8/ExceptionEx1.java
@@ -3,41 +3,77 @@ package ch8;
 /**
  * =================== Exception =======================
  * 
- * - 에러의 종류 : 컴파일 에러, 런타임 에러, 논리 에러
- * 	 컴파일 에러 : 컴파일시 발생하는 에러
- * 	 런타임 에러 : 실행시 발생하는 에러
- * 	 논리 에러 : 실행시 의도와 다르게 동작하는 에러
+ * - 에러의 종류 : 컴파일 에러, 런타임 에러, 논리 에러 
+ * 	 컴파일 에러 : 컴파일시 발생하는 에러 
+ *   런타임 에러 : 실행시 발생하는 에러
+ *   논리 에러 : 실행시 의도와 다르게 동작하는 에러
  * 
- * - ERROR vs Exception
- * 	 Error : 코드로 해결할 수 없는 심각한 오류
- * 	 Exception : 코드로 해결할 수 있는 다소 미약한 오류(컴파일 에러 X)
- * 				 일어날 수 있는 오류에 대비헤 JAVA에서 제공하는 문법을 통해 예외처리
+ * - ERROR vs Exception 
+ * 	 Error : 코드로 해결할 수 없는 심각한 오류 
+ *   Exception : 코드로 해결할 수 있는 다소 미약한 오류(컴파일 에러 X) 
+ *   			 일어날 수 있는 오류에 대비헤 JAVA에서 제공하는 문법을 통해 예외처리
  * 
  * - 예외 처리의 목적
  *   프로그램의 비정상 종료를 막고, 정상적인 실행상태를 유지(중요)
- *   
- * - 문법
- *   try-catch-finally
- *   
  * 
+ * - 문법 
+ *   try-catch-finally
+ * 
+ *   try { 
+ *   	예외 발생 가능성이 있는 코드 
+ *   } catch(Exception e) { 
+ *   	예외 처리 코드 작성 
+ *   } finally { 
+ *   	예외가 발생하던, 발생하지 않던 처리할 코드 작성 
+ *      ex. 프로그램 설치시 사용했던 설치 파일 삭제 
+ *          자원해제시 사용되는 메서드 close()
+ * 
+ *     모든 OS 입장에서의 자원에 대해서는 대부분 close() 함수 또는 메서드가 있음 
+ *     자원 : File, Socket, DB 등
+ * 
+ * 	   고려사항> 
+ *     자원 해제시, Finally의 복잡도가 높아질 가능성이 있음 
+ *     -> 해결> try... with... resources...
+ * 	   -> 해결을 위한 구문을 사용하기 위해선 AutoCloseable 인터페이스 구현해야 함
+ * 	   -> ExceptionEx5.class 참고
+ * 
+ *     자원해제. 
+ *     가능한 경우, 메모리에 자원 로딩이 되어 있는 상태인 경우 자원의 인스턴스가 null인지 체크한 후, null이면 close() 호출
  * 
  */
 public class ExceptionEx1 {
-	
+
 	public static void main(String[] args) {
+		
 		int num = 100;
+		
+		ExceptionEx1.method1(num);
+		System.out.println("method1() 호출 종료");
+		System.out.println("method1() 정상 종료");
+	}
+
+	static void method1(int num) {
+		System.out.println("method1() 호출됨");
+		
 		int result = 0;
 		
-		for(int i=0; i<10; i++) {
-			try {
-				result = num / (int)(Math.random()*10);	
+		try {
+			for (int i = 0; i < 10; i++) {
+				result = num / (int) (Math.random() * 10);
 				System.out.println(result);
 			}
-			catch (Exception e) {
-				System.err.println("ERROR Occured");
-				e.printStackTrace();
-			} 
+			
+			// return 이 있어도 finally문 반드시 실행
+			// 즉, 예외 유무와 관계없이 finally는 수행됨
+			return;
+		} catch (Exception e) {
+			System.err.println("ERROR Occured");
+			e.printStackTrace();
+		} finally {
+			System.out.println("method1(): Finally block");
 		}
 	}
+	
+	
 
 }
--- hello/src/ch8/ExceptionEx4.java
+++ hello/src/ch8/ExceptionEx4.java
@@ -22,6 +22,9 @@ public class ExceptionEx4 {
 	
 	public static void main(String[] args) {
 		
+		SpaceException se = new SpaceException("abla");
+		MemoryException me = new MemoryException("dfndsf");
+		
 		// 설치할 파일 경로
 		String orgFilePath = "D:/drive/orgFilePath";
 		String saveFilePath = "D:/drive/saveFilePath";
@@ -33,16 +36,16 @@ public class ExceptionEx4 {
 			// 1. 설치 메서드 호출
 			install.runToInstall(saveFilePath);
 			
-			// 파일 복사 및 임시 파일 삭제
+			// 파일 복사 및 원래 파일 삭제
 			fileAction.copyFile(orgFilePath, saveFilePath);
 			fileAction.deleteFile(orgFilePath);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			// 실패시 만든 새 경로 파일 삭제
 			e.printStackTrace();
-			fileAction.deleteFile(orgFilePath);
-		} 
-		
-		
+			fileAction.deleteFile(saveFilePath);	
+		} finally {
+			fileAction.close();
+		}
 	}
 	
 }
@@ -52,13 +55,16 @@ class InstallAction {
 	CheckOS checkOs = new CheckOsImpl();
 	
 	// 설치 시작
-	public void runToInstall(String filePath) throws Exception {
+	public void runToInstall(String filePath) throws SpaceException {
 		System.out.println("설치를 시작합니다.");
 		
 		// 디스크 및 메모리 용량 확인
-		checkOs.checkDisk(filePath);
-		checkOs.checkMemory();
-		
+		if(!checkOs.checkDisk(filePath)) {
+			throw new SpaceException("Fail to Install : Disk size is not enough");
+		}
+		if(!checkOs.checkMemory()) {
+			throw new MemoryException("Fail to Install : Memory size is not enough");
+		}
 	}
 }
 
@@ -66,38 +72,88 @@ class FileAction {
 	
 	// 파일 복사
 	public void copyFile(String oldfilePath, String newfilePath) {
-		System.out.println("파일을 복사합니다.");
+		System.out.println(oldfilePath + " 파일을 " + newfilePath + "로 복사합니다.");
 	}
 	
 	// 파일 삭제
 	public void deleteFile(String oldfilePath) {
-		System.out.println("파일을 삭제합니다.");
+		System.out.println(oldfilePath + " 파일을 삭제합니다.");
+	}
+	
+	// 파일 자원 해제
+	public void close() {
+		System.out.println("파일 자원 해제!");
 	}
 }
 
 interface CheckOS {
 	
 	// 디스크 확인
-	public void checkDisk(String newfilePath) throws Exception ;
+	public boolean checkDisk(String newfilePath) throws SpaceException ;
 	
 	// 메모리 확인
-	public void checkMemory() throws Exception;
+	public boolean checkMemory() throws RuntimeException;
 	
 }
 
 class CheckOsImpl implements CheckOS {
 
 	@Override
-	public void checkDisk(String newfilePath) throws Exception {
+	public boolean checkDisk(String newfilePath) throws SpaceException {
+		
+		/* 디스크 여유 공간 체크 로직 */
+		
 		System.out.println("디스크 용량 확인!");
+		
+		// false : 여유 공간 부족 -> 예외를 발생시켜야 하는 상황
+		// true : 디스크의 설치 공간 충분 -> 예외 발생 필요 X
+		return true;	
 	}
 
 	@Override
-	public void checkMemory() throws Exception {
+	public boolean checkMemory() throws RuntimeException {
+		
+		/* 메모리 여유 공간 체크 로직 */
+
 		System.out.println("메모리 용량 확인");
+		return false;
 	}
 	
 }
 
+
+/**
+ * 예외클래스 정의
+ * 
+ * - 사용자 예외 : 100개
+ * - 시스템 예외 : 100개
+ * - 서비스 예외': 100개
+ * - 운영 예외 : 100개
+ * 
+ * -> try-catch 400개를 만들어야 함.. 미친짓?
+ * -> 운영 초반에는 엄격하게 하는게 맞을 수 있지만, 안정화되고 운영이 잘되고 있는 상황에서,
+ * 	  신규개발시, 수정시 try-catch를 하지 않아도 되도록 개발 및 유지보수 정책을 수립할 수 있음
+ * 
+ * -> RuntimeException class를 사용하면 try-catch 사용할 필요 X
+ * 
+ * - checked, unchecked 옵션
+ *   checked : 컴파일러가 검증하겠다 -> Java에서 제공하는 Exception 클래스와의 관계를 맺어주자..(상속)
+ *   		   예외 처리를 강제하겠다는 의미 -> 직접 예외 처리 or 예외 위임 처리
+ *   unchecked : 컴파일러가 검사 X. -> RuntimeException 클래스와의 관계를 맺어주자..(상속)  
+ * 
+ */
+class SpaceException extends Exception {
+
+	public SpaceException(String msg) {
+		super(msg);
+	}
+}
+
+class MemoryException extends RuntimeException {
+
+	public MemoryException(String msg) {
+		super(msg);
+	}
+}
 
 
